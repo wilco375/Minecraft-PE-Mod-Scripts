8bf61fe756fc22a4c3aa49b33caf973376c39a5e
wilco375 <wilcovanbeijnum@gmail.com>
2014-05-05 10:38:00 +0200

    Update Thaumcraft Portable Hole Mod.js

--- Thaumcraft Portable Hole Mod.js
+++ Thaumcraft Portable Hole Mod.js
@@ -22,7 +22,7 @@ function useItem(x,y,z,itemId,blockId, side)
 		pHoleActive = 1
 		BlockSide = side
 		clientMessage("You clicked at " + x + " " + y + " " + z + " " + side);
-		if(BlockSide == 2 || 3){
+		if(BlockSide == 2){
 		block1 = getTile(pHoleX,pHoleY, pHoleZ)
 		block2 = getTile(pHoleX+1,pHoleY,pHoleZ)
 		block3 = getTile(pHoleX+1,pHoleY+1,pHoleZ)
@@ -41,7 +41,45 @@ function useItem(x,y,z,itemId,blockId, side)
 		setTile(pHoleX-1,pHoleY-1,pHoleZ,0)
 		setTile(pHoleX,pHoleY+1,pHoleZ,0)
 		setTile(pHoleX,pHoleY-1,pHoleZ,0)}
-		else if(BlockSide == 4 || 5){
+		else if(BlockSide == 3){
+		block1 = getTile(pHoleX,pHoleY, pHoleZ)
+		block2 = getTile(pHoleX+1,pHoleY,pHoleZ)
+		block3 = getTile(pHoleX+1,pHoleY+1,pHoleZ)
+		block4 = getTile(pHoleX+1,pHoleY-1,pHoleZ)
+		block5 = getTile(pHoleX-1,pHoleY,pHoleZ)
+		block6 = getTile(pHoleX-1,pHoleY+1,pHoleZ)
+		block7 = getTile(pHoleX-1,pHoleY-1,pHoleZ)
+		block8 = getTile(pHoleX,pHoleY+1,pHoleZ)
+		block9 = getTile(pHoleX,pHoleY-1,pHoleZ)
+		setTile(pHoleX,pHoleY, pHoleZ,0)
+		setTile(pHoleX+1,pHoleY,pHoleZ,0)
+		setTile(pHoleX+1,pHoleY+1,pHoleZ,0)
+		setTile(pHoleX+1,pHoleY-1,pHoleZ,0)
+		setTile(pHoleX-1,pHoleY,pHoleZ,0)
+		setTile(pHoleX-1,pHoleY+1,pHoleZ,0)
+		setTile(pHoleX-1,pHoleY-1,pHoleZ,0)
+		setTile(pHoleX,pHoleY+1,pHoleZ,0)
+		setTile(pHoleX,pHoleY-1,pHoleZ,0)}
+		else if(BlockSide == 4){
+		block1 = getTile(pHoleX,pHoleY, pHoleZ)
+		block2 = getTile(pHoleX,pHoleY,pHoleZ+1)
+		block3 = getTile(pHoleX,pHoleY+1,pHoleZ+1)
+		block4 = getTile(pHoleX,pHoleY-1,pHoleZ+1)
+		block5 = getTile(pHoleX,pHoleY,pHoleZ-1)
+		block6 = getTile(pHoleX,pHoleY+1,pHoleZ-1)
+		block7 = getTile(pHoleX,pHoleY-1,pHoleZ-1)
+		block8 = getTile(pHoleX,pHoleY+1,pHoleZ)
+		block9 = getTile(pHoleX,pHoleY-1,pHoleZ)
+		setTile(pHoleX,pHoleY, pHoleZ)
+		setTile(pHoleX,pHoleY,pHoleZ+1)
+		setTile(pHoleX,pHoleY+1,pHoleZ+1)
+		setTile(pHoleX,pHoleY-1,pHoleZ+1)
+		setTile(pHoleX,pHoleY,pHoleZ-1)
+		setTile(pHoleX,pHoleY+1,pHoleZ-1)
+		setTile(pHoleX,pHoleY-1,pHoleZ-1)
+		setTile(pHoleX,pHoleY+1,pHoleZ)
+		setTile(pHoleX,pHoleY-1,pHoleZ)	}	
+		else if(BlockSide == 5){
 		block1 = getTile(pHoleX,pHoleY, pHoleZ)
 		block2 = getTile(pHoleX,pHoleY,pHoleZ+1)
 		block3 = getTile(pHoleX,pHoleY+1,pHoleZ+1)
@@ -82,7 +120,7 @@ if((pHoleActive == 1))
 			} if(countdown == 0) {
 				clientMessage("Done!");
 				pHoleActive = 0;
-				if(BlockSide == 2 || 3){
+				if(BlockSide == 2){
 				setTile(pHoleX,pHoleY, pHoleZ,block1)
 				setTile(pHoleX+1,pHoleY,pHoleZ,block2)
 				setTile(pHoleX+1,pHoleY+1,pHoleZ,block3)
@@ -92,7 +130,27 @@ if((pHoleActive == 1))
 				setTile(pHoleX-1,pHoleY-1,pHoleZ,block7)
 				setTile(pHoleX,pHoleY+1,pHoleZ,block8)
 				setTile(pHoleX,pHoleY-1,pHoleZ,block9)}
-				else if(BlockSide == 4 || 5){
+				else if(BlockSide == 3){
+				setTile(pHoleX,pHoleY, pHoleZ,block1)
+				setTile(pHoleX+1,pHoleY,pHoleZ,block2)
+				setTile(pHoleX+1,pHoleY+1,pHoleZ,block3)
+				setTile(pHoleX+1,pHoleY-1,pHoleZ,block4)
+				setTile(pHoleX-1,pHoleY,pHoleZ,block5)
+				setTile(pHoleX-1,pHoleY+1,pHoleZ,block6)
+				setTile(pHoleX-1,pHoleY-1,pHoleZ,block7)
+				setTile(pHoleX,pHoleY+1,pHoleZ,block8)
+				setTile(pHoleX,pHoleY-1,pHoleZ,block9)}
+				else if(BlockSide == 4){
+				setTile(pHoleX,pHoleY, pHoleZ,block1)
+				setTile(pHoleX,pHoleY,pHoleZ+1,block2)
+				setTile(pHoleX,pHoleY+1,pHoleZ+1,block3)
+				setTile(pHoleX,pHoleY-1,pHoleZ+1,block4)
+				setTile(pHoleX,pHoleY,pHoleZ-1,block5)
+				setTile(pHoleX,pHoleY+1,pHoleZ-1,block6)
+				setTile(pHoleX,pHoleY-1,pHoleZ-1,block7)
+				setTile(pHoleX,pHoleY+1,pHoleZ,block8)
+				setTile(pHoleX,pHoleY-1,pHoleZ,block9)
+				else if(BlockSide == 5){
 				setTile(pHoleX,pHoleY, pHoleZ,block1)
 				setTile(pHoleX,pHoleY,pHoleZ+1,block2)
 				setTile(pHoleX,pHoleY+1,pHoleZ+1,block3)
