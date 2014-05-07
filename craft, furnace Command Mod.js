2f40b9ea6c34f50ac5ef4c8c5a8fa4ef5b4a1c2b
wilco375 <wilcovanbeijnum@gmail.com>
2014-05-07 17:41:34 +0200

    Update craft, furnace Command Mod.js

--- craft, furnace Command Mod.js
+++ craft, furnace Command Mod.js
@@ -7,14 +7,14 @@ Level.setGameMode(0)
 clientMessage(ChatColor.GOLD + "Changed gamemode to survival")
 Level.setTile(Player.getX()+1,Player.getY()-1,Player.getZ(),58);
 clientMessage(ChatColor.GOLD + "Spawned crafting table!");
-setRot(getPlayerEnt(),-90,70);
+setRot(getPlayerEnt(),270,70);
 }
 
 else if(cmd[0] == "craft" && Level.getGameMode() == "0")
 {
 Level.setTile(Player.getX()+1,Player.getY()-1,Player.getZ(),58);
 clientMessage(ChatColor.GOLD + "Spawned crafting table!");
-setRot(getPlayerEnt(),-90,70);
+setRot(getPlayerEnt(),270,70);
 }
 
 else if(cmd[0] == "furnace" && Level.getGameMode() == "1")
@@ -23,14 +23,14 @@ Level.setGameMode(0)
 clientMessage(ChatColor.GOLD + "Changed gamemode to survival") 
 Level.setTile(Player.getX()+1,Player.getY()-1,Player.getZ(),61);
 clientMessage(ChatColor.GOLD + "Spawned furnace!");
-setRot(getPlayerEnt(),-90,70);
+setRot(getPlayerEnt(),270,70);
 }
 
 else if(cmd[0] == "furnace" && Level.getGameMode() == "0")
 { 
 Level.setTile(Player.getX()+1,Player.getY()-1,Player.getZ(),61);
 clientMessage(ChatColor.GOLD + "Spawned furnace!");
-setRot(getPlayerEnt(),-90,70);
+setRot(getPlayerEnt(),270,70);
 }
 
 }
