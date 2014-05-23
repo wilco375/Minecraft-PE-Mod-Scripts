dde8bca581a1fc37d2cd0a1342c9ef5d9288e080
wilco375 <wilcovanbeijnum@gmail.com>
2014-05-23 17:56:07 +0200

    Create Trash Can v1.0vanilla.js

---
+++ Trash Can v1.0vanilla.js
@@ -0,0 +1,21 @@
+//Trash Can Mod v0.8.5
+//by wilco375
+
+var TrashCanId = 122
+
+Block.defineBlock(TrashCanId,"Trash Can", [["piston_botton",0],["piston_inner",0],["piston_bottom",0],["piston_bottom",0],["piston_bottom",0],["piston_bottom",0]] ,20,false,0)
+
+Block.setShape(TrashCanId,0.125,0,0.125,0.875,0.875,0.875)
+Block.setDestroyTime(TrashCanId, 6)
+Block.setColor(TrashCanId,[0x999999])
+Item.addCraftRecipe(TrashCanId,1,0,[54,1,0,4,5,0,1,3,0])
+Item.setCategory(TrashCanId,8,0);
+
+function useItem(x,y,z,itemId,blockId,side){
+if(blockId == TrashCanId){
+if(itemId == 0){
+clientMessage("You aren't holding anything")}
+else{
+preventDefault()
+Player.clearInventorySlot(Player.getSelectedSlotId())}
+}}
