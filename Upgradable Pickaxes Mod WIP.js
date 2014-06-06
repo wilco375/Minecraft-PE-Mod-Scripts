37c1f94c53137d33b1222f567034252b9cb98d5b
wilco375 <wilcovanbeijnum@gmail.com>
2014-06-06 18:30:21 +0200

    Update Upgradable Pickaxes Mod WIP.js

--- Upgradable Pickaxes Mod WIP.js
+++ Upgradable Pickaxes Mod WIP.js
@@ -1,7 +1,7 @@
 var AutoSmeltUpgradeId = 277 //(dia shovel, temp)
 var DoubletUpgradeId = 401
 
-var NPlayer={takeItem=function takeItem(i,a,d){addItemInventory(i,-a,d)},giveItem=function giveItem(i,a,d){addItemInventory(i,a,d)},takeHeldItem=function takeHeldItem(a){if(a=="all"){var p=Player.getCarriedItemAmount();var i=getCarriedItem();Entity.setCarriedItem(getPlayerEnt(),i,1,p)}else{var p=Player.getCarriedItemAmount()-amount;var i=getCarriedItem();Entity.setCarriedItem(getPlayerEnt(),i,1,p)}},tp=function tp(x,y,z){setPosition(getPlayerEnt(),x,y,z)},move=function move(x,y,z){var xx=Player.getX();var yy=Player.getY();var zz=Player.getZ();setPosition(getPlayerEnt(),x+xx,y+yy,z+zz)},getCoords=function getCoords(){var px=Player.getX;var py=Player.getY;var pz=Player.getZ;return px+", "+py+", "+pz},checkInventory=function checkInventory(itemId,data){var count=0;if(data==null){data=0}for(var i=0;i<27;i++){if(Player.getInventorySlot(i)==itemId&&Player.getInventoryData(i)==data){count+=Player.getSlotCount(i)}}return count}}
+var NPlayer={checkInventory=function checkInventory(itemId,data){var count=0;if(data==null){data=0}for(var i=0;i<27;i++){if(Player.getInventorySlot(i)==itemId&&Player.getInventoryData(i)==data){count+=Player.getSlotCount(i)}}return count}}
 
 function destroyBlock(x,y,z,shouldDropItem){
 	clientMessage("1")
