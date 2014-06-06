d93d4f30f2e41943fcdab8b831c478f58837bc1a
wilco375 <wilcovanbeijnum@gmail.com>
2014-06-06 19:12:29 +0200

    Update Upgradable Pickaxes Mod WIP.js

--- Upgradable Pickaxes Mod WIP.js
+++ Upgradable Pickaxes Mod WIP.js
@@ -1,30 +1,60 @@
 var AutoSmeltUpgradeId = 277 //(dia shovel, temp)
-var DoubleUpgradeId = 279 //(dia axe, temp)
+var PulveriseUpgradeId = 279 //(dia axe, temp)
 var ironDustId = 263 //(coal, temp)
+var goldDustId = 260 //(apples, temp)
 
 function destroyBlock(x,y,z,shouldDropItem){
-	clientMessage("1")
 	ci = getCarriedItem()
-	clientMessage(ci) 
 	if(ci == 257 || ci == 270 || ci == 274 || ci == 278 || ci == 285){
 		gt = getTile(x,y,z)
-		clientMessage("2")
-		clientMessage(Player.checkForInventoryItem(AutoSmeltUpgradeId))
 		if(Player.checkForInventoryItem(AutoSmeltUpgradeId) >= 1){
-			clientMessage("3")
+			//clientMessage("3")
 			if(gt == 15 && ci != 270){
-				clientMessage("4")
 				preventDefault()
 				setTile(x,y,z,0,0)
-				Level.dropItem(x,y,z,0.25,265,Player.checkForInventoryItem(DoubleUpgradeId)+1,0)
+				Level.dropItem(x,y,z,0.25,265,Player.checkForInventoryItem(PulveriseUpgradeId)+1,0)
 			}
+			if(gt == 14 && ci != 270 && ci != 274){
+				preventDefault()
+				setTile(x,y,z,0,0)
+				Level.dropItem(x,y,z,0.25,265,Player.checkForInventoryItem(PulveriseUpgradeId)+1,0)
+			}
+			if(gt == 73 || gt == 74){
+				if(ci != 270 && ci != 274 && Player.checkForInventoryItem(PulveriseUpgradeId) == 1){
+				preventDefault()
+				setTile(x,y,z,0,0)
+				Level.dropItem(x,y,z,0.25,331,8,0)
+				}
+			}
+			if(gt == 21 && ci != 270 && Player.checkForInventoryItem(PulveriseUpgradeId) == 1){
+				preventDefault()
+				setTile(x,y,z,0)
+				Level.dropItem(x,y,z,0.25,351,8,4)
+			}	
 		}
-		if(Player.checkForInventoryItem(DoubleUpgradeId) == 1 && Player.checkForInventoryItem(AutoSmeltUpgradeId) == 0){
+		
+		if(Player.checkForInventoryItem(PulveriseUpgradeId) == 1 && Player.checkForInventoryItem(AutoSmeltUpgradeId) == 0){
 			if(gt == 15 && ci != 270){
 				preventDefault()
 				setTile(x,y,z,0)
 				Level.dropItem(x,y,z,0.25,ironDustId,2,0)
+			if(gt == 15 && ci != 270 && ci != 274){
+				preventDefault()
+				setTile(x,y,z,0)
+				Level.dropItem(x,y,z,0.25,goldDustId,2,0)
 			}	
+			if(gt == 73 || gt == 74){
+				if(ci != 270 && ci != 274){
+				preventDefault()
+				setTile(x,y,z,0,0)
+				Level.dropItem(x,y,z,0.25,331,8,0)
+				}
+			if(gt == 21 && ci != 270){
+				preventDefault()
+				setTile(x,y,z,0)
+				Level.dropItem(x,y,z,0.25,351,8,4)
+			}	
+			}
 		}
 	}
 }
