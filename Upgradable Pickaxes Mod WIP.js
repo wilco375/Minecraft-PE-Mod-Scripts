73122098b82b2c85512c18c8e33731c036834e57
wilco375 <wilcovanbeijnum@gmail.com>
2014-06-06 16:56:11 +0200

    Update Upgradable Pickaxes Mod WIP.js

--- Upgradable Pickaxes Mod WIP.js
+++ Upgradable Pickaxes Mod WIP.js
@@ -2,10 +2,14 @@ var AutoSmeltUpgradeId = 277 //(dia shovel, temp)
 var DoubletUpgradeId = 401
 
 function destroyBlock(x,y,z){
+	clientMessage("1")
 	if(Player.getCarriedItem == 257 || Player.getCarriedItem == 270 || Player.getCarriedItem == 274 || Player.getCarriedItem == 278 || Player.getCarriedItem == 285){
+		clientMessage("2")	
 		if(Player.itemInInv(AutoSmeltUpgradeId) == 1){
 			gt = getTile(x,y,z)
+			clientMessage("3")
 			if(gt == 15){
+				clientMessage("4")
 				preventDefault()
 				setTile(x,y,z,0,0)
 				Level.dropItem(x,y,z,1,265,Player.itemInInv(DoubleUpgradeId)+1,0)
