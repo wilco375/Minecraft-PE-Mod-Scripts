a2ac20a25bd9bff346b3f71a04b0e645904e303a
wilco375 <wilcovanbeijnum@gmail.com>
2014-06-11 18:26:18 +0200

    Update Upgradable Tools Mod v0.2 Beta.js

--- Upgradable Tools Mod v0.2 Beta.js
+++ Upgradable Tools Mod v0.2 Beta.js
@@ -1,7 +1,6 @@
-//Upgradable Tools Mod v0.2 Beta
+//Upgradable ToolsMod 0.2 beta version
 //by wilco375 
 //Textures are from the Thermal Expantion Mod or created by myself
-
 
 //Item ID's:
 var ironDustId = 408
@@ -15,11 +14,8 @@ var EfficiencyUpgradeId = 415
 var SilkTouchUpgradeId = 416 
 
 
-
-
 //Only enable the efficiency upgrade if you have a good device, it causes a lot of lag because it has to track all kinds of things every tick to work
 var EfficiencyUpgradeOn = 0
-
 
 var Counter
 var Repair
@@ -37,28 +33,13 @@ var blockStartDestroying
 var blockDestroyed
 var EfficiencyOn
 
-//Block hardness lib
-var h1 = 1.5
-var h2 = 0.6
-var h3 = 0.5
-var h12 = 0.5
-var h13 = 0.6
-var h14 = 3
-var h15 = 3 
-var h16 = 3
-var h21 = 3
-var h56 = 3
-var h73 = 3
-var h74 = 3
-
-
-ModPE.setItem(AutoSmeltUpgradeId,"record_cat",0,"Auto-Smelt Upgrade")
-ModPE.setItem(PulveriseUpgradeId,"record_chirp",0,"Pulveriser Upgrade")
-ModPE.setItem(FortuneUpgradeId,"record_mellohi",0,"Fortune Upgrade")
-ModPE.setItem(RepairUpgradeId,"record_stal",0,"Repair Upgrade")
-ModPE.setItem(UnbreakingUpgradeId,"record_strad",0,"Unbreaking Upgrade")
-ModPE.setItem(EfficiencyUpgradeId,"record_wait",0,"Efficiency Upgrade")
-ModPE.setItem(SilkTouchUpgradeId,"record_ward",0,"Silk-Touch Upgrade")
+ModPE.setItem(AutoSmeltUpgradeId,"record_cat",0,"Auto-Smelt Pickaxe Upgrade")
+ModPE.setItem(PulveriseUpgradeId,"record_chirp",0,"Pulveriser Pickaxe Upgrade")
+ModPE.setItem(FortuneUpgradeId,"record_mellohi",0,"Fortune Pickaxe Upgrade")
+ModPE.setItem(RepairUpgradeId,"record_stal",0,"Repair Pickaxe Upgrade")
+ModPE.setItem(UnbreakingUpgradeId,"record_strad",0,"Unbreaking Pickaxe Upgrade")
+ModPE.setItem(EfficiencyUpgradeId,"record_wait",0,"Efficiency Pickaxe Upgrade")
+ModPE.setItem(SilkTouchUpgradeId,"record_ward",0,"Silk-Touch Pickaxe Upgrade")
 ModPE.setItem(ironDustId,"record_far",0,"Iron Dust")
 ModPE.setItem(goldDustId,"record_mall",0,"Gold Dust")
 Item.addFurnaceRecipe(ironDustId,265,0)
@@ -83,14 +64,11 @@ Item.setCategory(RepairUpgradeId,2)
 Item.setCategory(UnbreakingUpgradeId,2)
 Item.setCategory(SilkTouchUpgradeId,2)
 
-
 ModPE.overrideTexture("images/items-opaque.png", "http://i.imgur.com/PymUpLQ.png")
-
 
 function startDestroyBlock(){
 	blockStartDestroying = 1
 }
-
 
 function destroyBlock(x,y,z,shouldDropItem){
 	blockDestroyed = 1
@@ -135,59 +113,73 @@ function destroyBlock(x,y,z,shouldDropItem){
 			}
 		}
 	}
-
-
+	
 }
 
 
 
 
 
-
-
-
-
-
 function modTick(){
-	ci = Player.getCarriedItem()
-	if(ci == 257 || ci == 274 || ci == 270 || ci == 278 || ci == 285){
-		if(EfficiencyOn != 1){ 
-			if(Player.checkForInventoryItem(EfficiencyUpgradeId) <= 5 && Player.checkForInventoryItem(EfficiencyUpgradeId) != 0){
-				eu =  Player.checkForInventoryItem(EfficiencyUpgradeId)*0.3 +1
-				EfficiencyOn = 1
-				Block.setDestroyTime(1,h1/eu)
-				Block.setDestroyTime(2,h2/eu)
-				Block.setDestroyTime(3,h3/eu)
-				Block.setDestroyTime(12,h12/eu)
-				Block.setDestroyTime(13,h13/eu)
-				Block.setDestroyTime(14,h14/eu)
-				Block.setDestroyTime(15,h15/eu)
-				Block.setDestroyTime(16,h16/eu)
-				Block.setDestroyTime(21,h21/eu)
-				Block.setDestroyTime(56,h56/eu)
-				Block.setDestroyTime(73,h73/eu)
-				Block.setDestroyTime(74,h74/eu)
-			}
-		}
-	}
-	if(EfficiencyOn == 1){
-		if(ci != 257 || ci != 274 || ci != 270 || ci != 278 || ci != 285 || Player.checkForInventoryItem(EfficiencyUpgradeId) == 0){
-			EfficiencyOn = 0
-			Block.setDestroyTime(1,h1)
-			Block.setDestroyTime(2,h2)
-			Block.setDestroyTime(3,h3)
-			Block.setDestroyTime(12,h12)
-			Block.setDestroyTime(13,h13)
-			Block.setDestroyTime(14,h14)
-			Block.setDestroyTime(15,h15)
-			Block.setDestroyTime(16,h16)
-			Block.setDestroyTime(21,h21)
-			Block.setDestroyTime(56,h56)
-			Block.setDestroyTime(73,h73)
-			Block.setDestroyTime(74,h74)
-		}
-	}
 
+//Efficiency upgrade
+if(EfficiencyUpgradeOn == 1){
+	velX=(Player.getX()-lastX)/(1/20)
+	lastX=Player.getX()
+	velY=(Player.getY()-lastY)/(1/20)
+	lastY=Player.getY()
+	velZ=(Player.getZ()-lastZ)/(1/20)
+	lastZ=Player.getZ()
+	damage=(Player.getCarriedItemData()-lastDamage)
+	lastDamage=Player.getCarriedItemData()
+	
+	if(blockStartDestroying == 1){
+		destroyingBlock = 1
+	}
+	if(damage != 0){
+		destroyingBlock = 1
+	}
+	if(destroyingBlock == 1 && velX != 0){
+		destroyingBlock = 0
+		if(EfficiencyOn == 1){
+			ModPE.setGameSpeed(20)
+			EfficiencyOn = 0
+		}
+	}
+	if(destroyingBlock == 1 && velY != 0){
+		destroyingBlock = 0
+		if(EfficiencyOn == 1){
+			ModPE.setGameSpeed(20)
+			EfficiencyOn = 0
+		}
+	}	
+	if(destroyingBlock == 1 && velZ != 0){
+		destroyingBlock = 0
+		if(EfficiencyOn == 1){
+			ModPE.setGameSpeed(20)
+			EfficiencyOn = 0
+		}
+	}
+	if(Player.checkForInventoryItem(EfficiencyUpgradeId) >= 1 && destroyingBlock == 1){
+		ci = Player.getCarriedItem()
+		if(ci == 257 || ci == 274 || ci == 270 || ci == 278 || ci == 285){
+		if(EfficiencyOn != 1){
+		if(Player.checkForInventoryItem(EfficiencyUpgradeId) <= 5){
+			tickSpeed = Player.checkForInventoryItem(EfficiencyUpgradeId)*6+20
+		}
+		else{
+			tickSpeed = 50
+		}
+		ModPE.setGameSpeed(tickSpeed)
+		EfficiencyOn = 1
+		tickSpeed = 20
+		}
+		}
+	}
+	
+	blockStartDestroying = 0
+	blockDestroyed = 0
+}
 //Repair upgrade (Only works on selected item)
 	if(Counter == null){
 		Counter = 1
@@ -217,7 +209,6 @@ function modTick(){
 	}	
 }
 
-
 //The following custom function is created by Kyurem838 on the minecraft forums:
 //(Check out this topic for all the custom functions: http://goo.gl/xT7mFB)
 Player.checkForInventoryItem = function(id, amount, damage) {
@@ -228,7 +219,6 @@ Player.checkForInventoryItem = function(id, amount, damage) {
 	for(var i = 0; i < 255; i++) if(Player.getInventorySlot(i) == id && Player.getInventorySlotData(i) == damage) count += Player.getInventorySlotCount(i);
 	return count/2
 };
-
 
 //function that runs all the upgrades
 function runUpgrades(){
@@ -263,11 +253,10 @@ function runUpgrades(){
 			extraItem = 3
 		}
 	}
-	else if(Player.checkForInventoryItem(FortuneUpgradeId) == 0){
+	if(extraItem != 1 && extraItem != 2 && extraItem != 3){
 		extraItem = 0
 	}
-
-
+	
 	if(Player.checkForInventoryItem(SilkTouchUpgradeId) >= 1 && Player.checkForInventoryItem(FortuneUpgradeId) == 0 && Player.checkForInventoryItem(PulveriseUpgradeId) == 0 && Player.checkForInventoryItem(AutoSmeltUpgradeId) == 0){
 		gt = getTile(x,y,z)
 		if(gt == 16 || gt == 21 || gt == 73 || gt == 74 || gt == 56 || gt == 2 || gt == 79 || gt == 1 || gt == 80 || gt == 13 || gt == 82 || gt == 47 || gt == 20){
@@ -287,8 +276,7 @@ function runUpgrades(){
 			}
 		}
 	}
-
-
+	
 	if(Player.checkForInventoryItem(PulveriseUpgradeId) == 0 && Player.checkForInventoryItem(FortuneUpgradeId) >= 1){ //fortune, no pulverise (lapis, redstone)
 			if(gt == 73 || gt == 74){
 				if(ci != 270 && ci != 274){
@@ -305,8 +293,7 @@ function runUpgrades(){
 				Level.dropItem(x,y,z,0.25,351,6+(extraItem*8),4)
 			}
 		}
-
-
+	
 	if(ci == 257 || ci == 270 || ci == 274 || ci == 278 || ci == 285){
 		gt = getTile(x,y,z)
 		if(Player.checkForInventoryItem(AutoSmeltUpgradeId) >= 1){ //autosmelt and changes if also pulverise (gold and iron)
@@ -343,8 +330,7 @@ function runUpgrades(){
 				Level.dropItem(x,y,z,0.25,266,Player.checkForInventoryItem(PulveriseUpgradeId)+1+extraItem,0)
 			}
 		}
-
-
+		
 		if(Player.checkForInventoryItem(PulveriseUpgradeId) >= 1 && Player.checkForInventoryItem(AutoSmeltUpgradeId) == 0){ //Pulverise and no autosmelt (gold and iron)
 			if(gt == 15 && ci != 270){
 				preventDefault()
@@ -359,8 +345,7 @@ function runUpgrades(){
 				Level.dropItem(x,y,z,0.25,goldDustId,2+extraItem,0)
 			}	
 		}
-
-
+		
 		if(Player.checkForInventoryItem(PulveriseUpgradeId) >= 1){ //Pulveriser upgrade and optinally autosmelt (lapis and redstone)
 			if(gt == 73 || gt == 74){
 				if(ci != 270 && ci != 274){
@@ -379,9 +364,3 @@ function runUpgrades(){
 		}
 	}
 }
-
-
-
-
-
-
