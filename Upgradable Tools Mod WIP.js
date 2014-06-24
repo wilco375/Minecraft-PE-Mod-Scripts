f73a0b6ed505221b587468a4efd643d2c82283c7
wilco375 <wilcovanbeijnum@gmail.com>
2014-06-24 21:25:23 +0200

    Update Upgradable Tools Mod WIP.js

--- Upgradable Tools Mod WIP.js
+++ Upgradable Tools Mod WIP.js
@@ -251,6 +251,39 @@ function attackHook(attacker, victim){
         			}
         			x = Math.cos(hit*(Math.PI/180));
         			z = Math.sin(hit*(Math.PI/180));
+        			setVelX(v, x*1.5);
+        			setVelY(v, 1);
+        			setVelZ(v, z*1.5);
+        		}
+			else if(getYaw() > 0 && getYaw() < 360){
+        			var hit = getYaw()+90;
+            			XVel = Math.cos(hit*(Math.PI/180));
+            			ZVel= Math.sin(hit*(Math.PI/180));
+            			setVelX(v, XVel*1.5);
+            			setVelY(v, 1);
+            			setVelZ(v, ZVel*1.5);
+        		}
+        		else if( getYaw() >= 360){
+            			var hit= getYaw()+90;
+            			for(go=0; hit>=360; go++){
+        				hit -= 360;
+        			}
+            			XVel = Math.cos(hit*(Math.PI/180));
+            			ZVel = Math.sin(hit*(Math.PI/180));
+			        setVelX(v, XVel*1.5);
+            			setVelY(v, 1);
+            			setVelZ(v, ZVel*1.5);
+          
+    			}
+		}
+		else if(Player.checkForInventoryItem(KnockbackUpgradeId) == 2){
+			if(getYaw() < 0){
+				var hit = getYaw()+90;
+        			for(go=0; hit<0; go++){
+        				hit+= 360;
+        			}
+        			x = Math.cos(hit*(Math.PI/180));
+        			z = Math.sin(hit*(Math.PI/180));
         			setVelX(v, x*2);
         			setVelY(v, 1);
         			setVelZ(v, z*2);
@@ -276,39 +309,6 @@ function attackHook(attacker, victim){
           
     			}
 		}
-		else if(Player.checkForInventoryItem(KnockbackUpgradeId) == 2){
-			if(getYaw() < 0){
-				var hit = getYaw()+90;
-        			for(go=0; hit<0; go++){
-        				hit+= 360;
-        			}
-        			x = Math.cos(hit*(Math.PI/180));
-        			z = Math.sin(hit*(Math.PI/180));
-        			setVelX(v, x*3);
-        			setVelY(v, 1);
-        			setVelZ(v, z*3);
-        		}
-			else if(getYaw() > 0 && getYaw() < 360){
-        			var hit = getYaw()+90;
-            			XVel = Math.cos(hit*(Math.PI/180));
-            			ZVel= Math.sin(hit*(Math.PI/180));
-            			setVelX(v, XVel*3);
-            			setVelY(v, 1);
-            			setVelZ(v, ZVel*3);
-        		}
-        		else if( getYaw() >= 360){
-            			var hit= getYaw()+90;
-            			for(go=0; hit>=360; go++){
-        				hit -= 360;
-        			}
-            			XVel = Math.cos(hit*(Math.PI/180));
-            			ZVel = Math.sin(hit*(Math.PI/180));
-			        setVelX(v, XVel*3);
-            			setVelY(v, 1);
-            			setVelZ(v, ZVel*3);
-          
-    			}
-		}
 		else if(Player.checkForInventoryItem(KnockbackUpgradeId) >= 3){
 			if(getYaw() < 0){
 				var hit = getYaw()+90;
@@ -317,17 +317,17 @@ function attackHook(attacker, victim){
         			}
         			x = Math.cos(hit*(Math.PI/180));
         			z = Math.sin(hit*(Math.PI/180));
-        			setVelX(v, x*4);
+        			setVelX(v, x*2.5);
         			setVelY(v, 1);
-        			setVelZ(v, z*4);
+        			setVelZ(v, z*2.5);
         		}
 			else if(getYaw() > 0 && getYaw() < 360){
         			var hit = getYaw()+90;
             			XVel = Math.cos(hit*(Math.PI/180));
             			ZVel= Math.sin(hit*(Math.PI/180));
-            			setVelX(v, XVel*4);
+            			setVelX(v, XVel*2.5);
             			setVelY(v, 1);
-            			setVelZ(v, ZVel*4);
+            			setVelZ(v, ZVel*2.5);
         		}
         		else if( getYaw() >= 360){
             			var hit= getYaw()+90;
@@ -336,9 +336,9 @@ function attackHook(attacker, victim){
         			}
             			XVel = Math.cos(hit*(Math.PI/180));
             			ZVel = Math.sin(hit*(Math.PI/180));
-			        setVelX(v, XVel*4);
+			        setVelX(v, XVel*2.5);
             			setVelY(v, 1);
-            			setVelZ(v, ZVel*4);
+            			setVelZ(v, ZVel*2.5);
           
     			}
 		}
