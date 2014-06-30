1bee4700e2cccfbf587f6ad1aab8faa1e52ecb02
wilco375 <wilcovanbeijnum@gmail.com>
2014-06-30 12:50:36 +0200

    Update Ethereal Glass.js

--- Ethereal Glass.js
+++ Ethereal Glass.js
@@ -9,7 +9,7 @@ var unstableIngotId = 456
 var lastX
 var lastZ
 
-ModPE.setTerrain(http://i.imgur.com/siiGzEm.png)
+ModPE.overrideTexture("images/terrain-atlas.tga", "http://i.imgur.com/siiGzEm.png")
 Block.defineBlock(EtherealGlassId,"Ethereal Glass ",["mushroom_block_inside",1],20,false,0);
 Block.setRenderLayer(EtherealGlassId, 1); 
 Item.addCraftRecipe(EtherealGlassId,1,0,[20,8,0,unstableIngotId,1,0])
