c2c59a0c742aecc92c77e16373c81f68dda5ab4f
wilco375 <wilcovanbeijnum@gmail.com>
2014-06-30 12:29:01 +0200

    Update Ultra Miner.js

--- Ultra Miner.js
+++ Ultra Miner.js
@@ -5,7 +5,7 @@
 var UltraMinerId = 250
 var giveUltraMiner
 
-ModPE.setItem(UltraMinerId,"fireworks_charge_overlay",0,"Ultra Miner")
+ModPE.setItem(UltraMinerId,"fireworks_charge_overlay",0,"Ultra Miner",1)
 ModPE.overrideTexture("images/items-opaque.png", "http://i.imgur.com/trFNulJ.png")
 Item.addCraftRecipe(250,1,0,[278,2,0, 289,7,0]);
 
