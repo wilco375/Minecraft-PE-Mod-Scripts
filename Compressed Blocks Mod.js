2c295a2033888500571c67b64afc900f919d7c3e
wilco375 <wilcovanbeijnum@gmail.com>
2014-09-01 12:18:32 +0200

    Update Compressed Blocks Mod.js

--- Compressed Blocks Mod.js
+++ Compressed Blocks Mod.js
@@ -1,6 +1,42 @@
 //Compressed Blocks Mod
 //by wilco375
 
-Block.defineBlock(176,"Compressed Cobblestone",["cobblestone",0],10,0,0)
+Block.defineBlock(176,"Compressed Cobblestone",["cobblestone",0],4,0,0)
 Block.setColor(176,[0x989898])
 Item.addCraftRecipe(176,1,0,[4,9,0])
+Block.setDestroyTime(176,2.1)
+
+Block.defineBlock(176,"Compressed Cobblestone",["cobblestone",0],4,0,0)
+Block.setColor(176,[0x989898])
+Item.addCraftRecipe(176,1,0,[4,9,0])
+Block.setDestroyTime(176,2.1)
+
+Block.defineBlock(176,"Compressed Cobblestone",["cobblestone",0],4,0,0)
+Block.setColor(176,[0x989898])
+Item.addCraftRecipe(176,1,0,[4,9,0])
+Block.setDestroyTime(176,2.1)
+
+Block.defineBlock(176,"Compressed Cobblestone",["cobblestone",0],4,0,0)
+Block.setColor(176,[0x989898])
+Item.addCraftRecipe(176,1,0,[4,9,0])
+Block.setDestroyTime(176,2.1)
+
+Block.defineBlock(176,"Compressed Cobblestone",["cobblestone",0],4,0,0)
+Block.setColor(176,[0x989898])
+Item.addCraftRecipe(176,1,0,[4,9,0])
+Block.setDestroyTime(176,2.1)
+
+Block.defineBlock(176,"Compressed Cobblestone",["cobblestone",0],4,0,0)
+Block.setColor(176,[0x989898])
+Item.addCraftRecipe(176,1,0,[4,9,0])
+Block.setDestroyTime(176,2.1)
+
+Block.defineBlock(176,"Compressed Cobblestone",["cobblestone",0],4,0,0)
+Block.setColor(176,[0x989898])
+Item.addCraftRecipe(176,1,0,[4,9,0])
+Block.setDestroyTime(176,2.1)
+
+Block.defineBlock(176,"Compressed Cobblestone",["cobblestone",0],4,0,0)
+Block.setColor(176,[0x989898])
+Item.addCraftRecipe(176,1,0,[4,9,0])
+Block.setDestroyTime(176,2.1)
