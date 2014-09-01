2fc5475708d46c75d87f6d13bdc87d6021a83f49
wilco375 <wilcovanbeijnum@gmail.com>
2014-09-01 11:46:34 +0200

    Update Compressed Blocks Mod.js

--- Compressed Blocks Mod.js
+++ Compressed Blocks Mod.js
@@ -2,4 +2,5 @@
 //by wilco375
 
 Block.defineBlock(176,"Compressed Cobblestone",["cobblestone",0],10,0,0)
-Block.setColor(176,[0x000000])
+Block.setColor(176,[0x989898])
+Item.addCraftRecipe(176,1,0,[4,9,0])
