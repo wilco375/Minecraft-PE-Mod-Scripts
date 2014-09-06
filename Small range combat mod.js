5de1b59136fc6e36bd07a49c29d563f8cf9a5703
wilco375 <wilcovanbeijnum@gmail.com>
2014-09-06 15:54:15 +0200

    Create Small range combat mod.js

---
+++ Small range combat mod.js
@@ -0,0 +1,25 @@
+//Small combat range mod
+//by wilco375
+
+function attackHook(a,v){
+if(Level.getGameMode() != 1){
+mx = Entity.getX(v)
+my = Entity.getY(v)
+mz = Entity.getZ(v)
+px = Player.getX()
+py = Player.getY()
+pz = Player.getZ() 
+if(mx < px-2 || mx > px+2){
+ preventDefault()
+ //clientMessage("This mob is too far away to hit")
+}
+else if(my < py-3 || my > py+3) {
+ preventDefault()
+ //clientMessage("This mob is too far away to hit")
+}
+else if(mz < pz-2 || mz > pz+2) {
+ preventDefault()
+ //clientMessage("This mob is too far away to hit")
+}
+}
+}
