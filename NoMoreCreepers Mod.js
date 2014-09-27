53619a0f315cb76c6b9a4bdddba90b175dee2708
wilco375 <wilcovanbeijnum@gmail.com>
2014-09-27 14:51:19 +0200

    Update NoMoreCreepers Mod.js

--- NoMoreCreepers Mod.js
+++ NoMoreCreepers Mod.js
@@ -1,3 +1,7 @@
+//NoMoreCreepers Mod
+//by wilco375
+//Don't share or redistribute this mod using the Github link, instead, use this link: http://adf.ly/rw0qU
+
 function entityAddedHook(entity){
 if(Entity.getEntityTypeId(entity)== 33){
 Entity.remove(entity)
