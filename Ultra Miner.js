f6d323046a12ef6371fc6704bdbb975e2fcbbb92
wilco375 <wilcovanbeijnum@gmail.com>
2014-09-27 14:58:24 +0200

    Update Ultra Miner.js

--- Ultra Miner.js
+++ Ultra Miner.js
@@ -1,6 +1,7 @@
 //UltraMiner Mod
 //by wilco375
 //Idea by Zipperink on the MC Forums (Thanks for the tip/idea)
+//Don't share or redistribute this mod using the Github link, instead, use this link: http://adf.ly/ohCoA
 
 var UltraMinerId = 250
 var giveUltraMiner
