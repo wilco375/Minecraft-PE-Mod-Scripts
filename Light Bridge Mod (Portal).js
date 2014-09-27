f59fcdb8fd08d554f5ef52166ae0a8006f5d173a
wilco375 <wilcovanbeijnum@gmail.com>
2014-09-27 14:48:19 +0200

    Update Light Bridge Mod (Portal).js

--- Light Bridge Mod (Portal).js
+++ Light Bridge Mod (Portal).js
@@ -1,5 +1,6 @@
 //Light Bridge Mod
 //by wilco375
+//Don't share or redistribute this mod using the Github link, instead, use this link: http://adf.ly/o52Pj
 
 var LightBridgeMakerId = 150
 var LightBridgeId = 151
