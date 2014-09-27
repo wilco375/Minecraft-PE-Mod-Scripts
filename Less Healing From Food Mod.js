e0a30df2e4a6cfff5beaf86fdd725418a80a0c95
wilco375 <wilcovanbeijnum@gmail.com>
2014-09-27 20:01:08 +0200

    Update Less Healing From Food Mod.js

--- Less Healing From Food Mod.js
+++ Less Healing From Food Mod.js
@@ -1,6 +1,6 @@
 //Less healing from food mod
 //by wilco375
-//Don't share or redistribute this mod using the Github link, instead, use this link:
+//Don't share or redistribute this mod using the Github link, instead, use this link: http://adf.ly/sPNyT
 
 var lastHealth, lastCarriedItemId
 
