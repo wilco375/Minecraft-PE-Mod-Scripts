cbd5dda27e3d402252a8103c0beb7babff6fd707
wilco375 <wilcovanbeijnum@gmail.com>
2014-09-29 18:58:23 +0200

    Update Chance of blocks not dropping mod.js

--- Chance of blocks not dropping mod.js
+++ Chance of blocks not dropping mod.js
@@ -1,6 +1,6 @@
 //Chance of blocks not dropping mod
 //by wilco375
-//Don't share or redistribute this mod using the Github link, instead, use this link: 
+//Don't share or redistribute this mod using the Github link, instead, use this link: http://adf.ly/sSqZO
 
 function destroyBlock(x,y,z){
  rnd = Math.floor((Math.random() * 50) + 1)
