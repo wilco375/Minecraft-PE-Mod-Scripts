009cfd43b2728eddc318d9b5538db8a91a3018f1
wilco375 <wilcovanbeijnum@gmail.com>
2015-01-13 18:47:25 +0100

    Update Alternate_Furnace_Power_Mod_Dev_Version.js

--- Alternate_Furnace_Power_Mod_Dev_Version.js
+++ Alternate_Furnace_Power_Mod_Dev_Version.js
@@ -39,7 +39,7 @@ Block.setColor(uraniumOre,[0x00FF00])
 Block.defineBlock(SolarPanelId,"Solar Panel",[["iron_block",0],["lapis_block",0],["iron_block",0],["iron_block",0],["iron_block",0],["iron_block",0]],20,1,0)
 Item.addCraftRecipe(SolarPanelId,1,0,[22,3,0,265,6,0])
 Block.defineBlock(reactorId,"Reactor",["iron_block",0],20,1,0)
-Item.addCraftRecipe(reactorId,1,0,[265,1,0,42,1,0,265,2,0,351,1,10,265,2,0,42,1,0,265,1,0])
+Item.addCraftRecipe(reactorId,1,0,[265,1,0,42,1,0,265,2,0,uraniumId,1,0,265,2,0,42,1,0,265,1,0])
 ModPE.setItem(uraniumId,"dye_powder",10,"Uranium");
 
 /////////////////////////////////
