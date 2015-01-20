a2e8a357c17d885e665988fe0b332f9ce5635204
wilco375 <wilcovanbeijnum@gmail.com>
2015-01-20 13:20:26 +0100

    Update Regen_Armor.js

--- Regen_Armor.js
+++ Regen_Armor.js
@@ -16,16 +16,22 @@ Item.addShapedRecipe(305, 1, 0, ["   ","r r","r r"], ["r",42, 0, "l",334, 0]);
 Item.addShapedRecipe(303, 1, 0, ["e e","rrr","rrr"], ["r",42, 0, "e",133, 0]);
 Item.addShapedRecipe(302, 1, 0, ["rer","r r","   "], ["r",42, 0, "e",133, 0]);
 
+function newWorld(){
+	clientMessage("Say something when the player joins a world")
+}
+
 function modTick(){
 	health = Entity.getHealth(getPlayerEnt())
-	if(health != 0){
-		//Check if the player is hurt
-		if(health < previousHealth){
-			if(previousHealth-health > 5){
-				Player.setHealth(health+5)
-			}
-			else{
-				Player.setHealth(previousHealth)
+	if(Player.getArmorSlot(0)+Player.getArmorSlot(1)+Player.getArmorSlot(2)+Player.getArmorSlot(3)==1214){
+		if(health != 0){
+			//Check if the player is hurt
+			if(health < previousHealth){
+				if(previousHealth-health > 5){
+					Player.setHealth(health+5)
+				}
+				else{
+					Player.setHealth(previousHealth)
+				}
 			}
 		}
 	}
