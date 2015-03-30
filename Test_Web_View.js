9c3f7997f04bf097bf1a9dc8252e9b974f15bd05
wilco375 <wilcovanbeijnum@gmail.com>
2015-03-30 14:39:49 +0200

    Update Test_Web_View.js

--- Test_Web_View.js
+++ Test_Web_View.js
@@ -1,3 +1,6 @@
+var webView;
+var ctx;
+
 function newLevel(){
 	ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
 	ctx.runOnUiThread(new java.lang.Runnable({ run: function() {
@@ -5,12 +8,13 @@ function newLevel(){
 			simpleGUI = new android.widget.PopupWindow();
 			var layout = new android.widget.RelativeLayout(ctx);
 			webView = new android.widget.WebView(ctx);
-			webView.loadUrl("http://www.google.com/").reload();
 			layout.addView(webView);
 			simpleGUI.setContentView(layout);
 			simpleGUI.setWidth(1000);
 			simpleGUI.setHeight(60);
-			simpleGUI.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.LEFT | android.view.Gravity.TOP, 10, 50);
+			simpleGUI.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.LEFT | android.view.Gravity.TOP, 500, 250);
+			
+			webView.loadUrl("http://www.google.com/").reload();
 		}catch(err){
 			print("Error: "+err);
 		}
