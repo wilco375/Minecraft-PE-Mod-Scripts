cb88ac1ce4af9c357cb3b33cd11b45218d2f8344
wilco375 <wilcovanbeijnum@gmail.com>
2015-03-30 15:06:20 +0200

    Update Portal Gun Mod Long Fall Boots.js

--- Portal Gun Mod Long Fall Boots.js
+++ Portal Gun Mod Long Fall Boots.js
@@ -1,48 +1,58 @@
-//Long Fall Boots Mod
-//by wilco375
-//Don't share or redistribute this mod using the Github link, instead, use this link: http://adf.ly/nBuX7
+/*
+Focus of: 
+- Portable Hole
+- Harming (6 DMG)
+- Harming II (10 DMG)
+- Healing
+- Floating
+- Teleportation
+*/
 
-tickcount = 0
-watertickcount = 0
-ModPE.langEdit("item.bootsChain.name","Long Fall Boots");
-ModPE.setItem(460,"hopper",0,"Long Fall Boot")
-Item.addCraftRecipe(460,1,0, [265,6,0, 1,1,0, 264,2,0])
-Item.setCategory(460,4)
-Item.addCraftRecipe(305,1,0, [460,2,0])
-ModPE.overrideTexture("images/items-opaque.png", "http://i.imgur.com/OYznGRZ.png ")
-ModPE.overrideTexture("images/armor/chain_1.png", " http://i.imgur.com/M1tWIjs.png ")
+var activeFocus = "floating";
+var y;
+var prevY;
+
+var tickcount = 0
+var watertickcount = 0
+
+ModPE.setItem(510,"stick",0,"Wizard Staff");
 
 function modTick(){
-PY3 = getPlayerY() -1
-PY4 = getPlayerY() -4
-PY1 = getPlayerY() -3
-PY2 = getPlayerY() -5
-PZ1 = getPlayerZ()
-PX1 = getPlayerX()
-Block1 = getTile(PX1, PY1, PZ1)
-Block2 = getTile(PX1, PY2, PZ1)
-Block3 = getTile(PX1, PY4, PZ1)
-boots = Player.getArmorSlot(3)
-
-if(boots == 305 && Block1 == 0 && Block2 != 0 && Block3 == 0 && tickcount == 0 && Level.getGameMode() == 0){
-setTile(PX1,PY3,PZ1,9,0)
-watertickcount = 1
-waterx = PX1
-watery = PY3
-waterz = PZ1
-tickcount = 1
+	//Floating
+	y = getPlayerY();
+	if(getPlayerY != null){
+		if(prevY > y){
+			Entity.setVelY(getPlayerEnt(),-0.5);
+			PY1 = getPlayerY() -3; PY2 = getPlayerY() -5; PY3 = getPlayerY() -1; PY4 = getPlayerY() -4;  PZ1 = getPlayerZ(); PX1 = getPlayerX();
+			Block1 = getTile(PX1, PY1, PZ1); Block2 = getTile(PX1, PY2, PZ1); Block3 = getTile(PX1, PY4, PZ1);
+			if(Block1 == 0 && Block2 != 0 && Block3 == 0 && tickcount == 0 && Level.getGameMode() == 0){
+				setTile(PX1,PY3,PZ1,9,0);
+				watertickcount = 1; waterx = PX1; watery = PY3; waterz = PZ1;
+				tickcount = 1;
+			}
+			if(watertickcount == 2){
+				setTile(waterx,watery,waterz,0,0);
+				watertickcount = 0;
+			}
+			
+			if(tickcount !=0){
+				if(tickcount != 10){
+					tickcount++;
+				}
+				else if(tickcount == 10){
+					tickcount = 0;
+				}
+			}
+			if(watertickcount != 0){
+				watertickcount++;
+			}
+		}
+	}
+	prevY = getPlayerY();
+	
+	//-----------
 }
-if(watertickcount == 2){
-setTile(waterx,watery,waterz,0,0)
-watertickcount = 0}
 
-
-if(tickcount !=0){
-if(tickcount != 10){
-tickcount++}
-else if(tickcount == 10){
-tickcount = 0}}
-
-if(watertickcount != 0){
-watertickcount++}
+function useItem(x,y,z,itemId,blockId,side){
+	
 }
