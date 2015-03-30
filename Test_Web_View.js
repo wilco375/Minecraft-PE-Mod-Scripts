86f48d59dac27d834e99223fae5765637eb7fd1d
wilco375 <wilcovanbeijnum@gmail.com>
2015-03-30 14:32:14 +0200

    Update Test_Web_View.js

--- Test_Web_View.js
+++ Test_Web_View.js
@@ -1,32 +1,21 @@
 function newLevel(){
+if(enabled != 1){
+enabled = 1
 	ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
 	ctx.runOnUiThread(new java.lang.Runnable({ run: function() {
 		try{
-			popupWindow = new android.widget.PopupWindow();
+			simpleGUI = new android.widget.PopupWindow();
 			var layout = new android.widget.RelativeLayout(ctx);
-			
-			size = new android.graphics.Point();
-			((android.app.Activity) ctx).getWindowManager().getDefaultDisplay().getSize(size);
-			width = size.x;
-			height = size.y;
-		
 			webView = new android.widget.WebView(ctx);
-			
-			webSettings = webView.getSettings();
-			webSettings.setJavaScriptEnabled(true);
-			webSettings.setBuiltInZoomControls(true);
-			webSettings.setSupportZoom(true);
-			
-			webView.loadUrl("http://www.google.com/");
-			webView.reload();
+			webView.loadUrl("http://www.google.com/").reload();
 			layout.addView(webView);
-			popupWindow.setContentView(layout);
-			popupWindow.setWidth(width-50);
-			popupWindow.setHeight(height-50);
-			popupWindow.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.LEFT | android.view.Gravity.TOP, 25, 25);
-			
-		} catch(err) {
+			simpleGUI.setContentView(layout);
+			simpleGUI.setWidth(1000);
+			simpleGUI.setHeight(60);
+			simpleGUI.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.LEFT | android.view.Gravity.TOP, 10, 50);
+		}catch(err){
 			print("Error: "+err);
 		}
 	} }));
+}
 }
