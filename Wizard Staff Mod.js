547c9da7f0329a7bb28dacee313b5d72b53bc2af
wilco375 <wilcovanbeijnum@gmail.com>
2015-03-30 16:26:01 +0200

    Update Wizard Staff Mod.js

--- Wizard Staff Mod.js
+++ Wizard Staff Mod.js
@@ -9,13 +9,15 @@ Focus of:
 */
 
 var selectedFocus
-var activeFocus = "shielding";
+var activeFocus = "floating";
 var y;
 var prevY;
 
 //Floating Variables
 var tickcount = 0
 var watertickcount = 0
+var floatPrevHealth
+var falling = false
 
 //Portable Hole Variables
 var pHoleId = 450
@@ -36,30 +38,18 @@ function modTick(){
 		y = getPlayerY();
 		if(getPlayerY != null){
 			if(prevY > y){
-				Entity.setVelY(getPlayerEnt(),-0.5);
-				PY1 = getPlayerY() -3; PY2 = getPlayerY() -5; PY3 = getPlayerY() -1; PY4 = getPlayerY() -4;  PZ1 = getPlayerZ(); PX1 = getPlayerX();
-				Block1 = getTile(PX1, PY1, PZ1); Block2 = getTile(PX1, PY2, PZ1); Block3 = getTile(PX1, PY4, PZ1);
-				if(Block1 == 0 && Block2 != 0 && Block3 == 0 && tickcount == 0 && Level.getGameMode() == 0){
-					setTile(PX1,PY3,PZ1,9,0);
-					watertickcount = 1; waterx = PX1; watery = PY3; waterz = PZ1;
-					tickcount = 1;
+				if(!falling){
+					Entity.setVelY(getPlayerEnt(),-0.2);
+					Player.setHealth(10000)
+					floatPrevHealth = Entity.getHealth(getPlayerEnt())
 				}
-				if(watertickcount == 2){
-					setTile(waterx,watery,waterz,0,0);
-					watertickcount = 0;
+				else{
+					Entity.setVelY(getPlayerEnt(),-0.2)
 				}
-				
-				if(tickcount !=0){
-					if(tickcount != 10){
-						tickcount++;
-					}
-					else if(tickcount == 10){
-						tickcount = 0;
-					}
-				}
-				if(watertickcount != 0){
-					watertickcount++;
-				}
+			}
+			else if(falling){
+				falling = false
+				Player.setHealth(floatPrevHealth)
 			}
 		}
 	}
@@ -161,10 +151,10 @@ function useItem(x,y,z,itemId,blockId,side){
 	//More Shielding Code
 	if(activeFocus == "shielding"){
 		if(shieldCoolDown > 0 && shieldCoolDown < 600){
-			clientMessage("Shield is on cooldown, please wait "+(shieldCoolDown/20)+" more seconds")
+			clientMessage("Shield is on cooldown, please wait "+Math.round(shieldCoolDown/20)+" more seconds")
 		}
 		else if(shieldCountDown > 0 && shieldCoolDown < 200){
-			clientMessage("Shield is already active. It will last "+(shieldCountDown/20)+ " more seconds")
+			clientMessage("Shield is already active. It will last "+Math.round(shieldCountDown/20)+ " more seconds")
 		}
 		else{
 			shieldActive = true
