5156a258359236cf46507d09b96a8397cce2a51d
wilco375 <wilcovanbeijnum@gmail.com>
2015-03-30 14:20:31 +0200

    Create Test_Web_View.js

---
+++ Test_Web_View.js
@@ -0,0 +1,32 @@
+function newLevel(){
+	ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+	ctx.runOnUiThread(new java.lang.Runnable({ run: function() {
+		try{
+			popupWindow = new android.widget.PopupWindow();
+			var layout = new android.widget.RelativeLayout(ctx);
+			
+			size = new android.graphics.Point();
+			getWindowManager().getDefaultDisplay().getSize(size);
+			width = size.x;
+			height = size.y;
+		
+			webView = new android.widget.WebView(ctx);
+			
+			webSettings = webView.getSettings();
+			webSettings.setJavaScriptEnabled(true);
+			webSettings.setBuiltInZoomControls(true);
+			webSettings.setSupportZoom(true);
+			
+			webView.loadUrl("http://www.google.com/");
+			webView.reload();
+			layout.addView(webView);
+			popupWindow.setContentView(layout);
+			popupWindow.setWidth(width-50);
+			popupWindow.setHeight(height-50);
+			popupWindow.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.LEFT | android.view.Gravity.TOP, 25, 25);
+			
+		} catch(err) {
+			print("Error: "+err);
+		}
+	} }));
+}
